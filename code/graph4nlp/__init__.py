31bf55cb2220f130a4eb946f7f17c0f0f0209d1b
ZhaoLiEric <ericlee19930210@gmail.com>
2022-02-03 03:27:13 -0500

    Add files via upload

---
+++ code/graph4nlp/__init__.py
@@ -0,0 +1,7 @@
+from . import data, datasets
+from .data import log_level
+from .modules import (
+    graph_construction,
+    graph_embedding,
+    utils,
+)
---
+++ code/graph4nlp/datasets/__init__.py
@@ -0,0 +1,6 @@
+"""
+graph4nlp.datasets module contains various common datasets implemented based on \
+    graph4nlp.data.dataset.
+"""
+
+from .MRD import MRDDataset
